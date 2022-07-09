7103a4fd584f78c355fc0007ac2436b1e17aa4e0
merlinthemagic <github@martinpetermadsen.com>
2022-07-09 15:38:49 +0100

    added support for circumventing the default config prompt

--- Src/Tools/Shells/RouterOs/PasswordAuthentication.php
+++ Src/Tools/Shells/RouterOs/PasswordAuthentication.php
@@ -30,6 +30,7 @@ class PasswordAuthentication extends PublicKeyAuthentication
 				"Microsoft Corporation"								=> "windows",
 				"\[".$rawUser."\@(.+?)\] \>"						=> "routeros",
 				"Do you want to see the software license\?"			=> "routeros",
+				"remove it, you will be disconnected\."				=> "routeros",
 				"No route to host"									=> "error",
 				"Connection timed out"								=> "error",
 				$userName . "@"										=> "linux"
@@ -67,18 +68,55 @@ class PasswordAuthentication extends PublicKeyAuthentication
 			return \MTM\SSH\Factories::getShells()->getBashTool()->getDestinationShell($ctrlObj, $userName);
 			
 		} elseif ($rType == "routeros") {
-			
+			if ($rValue == "remove it, you will be disconnected\.") {
+				//we are the only ones with the information needed to clear the prompt of the question regarding default config
+				//if we dont clear it here the Destination function will have a hell of a time figuring out whats going on
+				$strCmd	= "n";
+				$regEx	= "(" . implode("|", array_keys($regExs)) . ")";
+				$cmdObj		= $ctrlObj->getCmd($strCmd, $regEx, $timeout);
+				$cmdObj->get();
+				$data		= $cmdObj->getReturnData(); //need return data so the prompt is not stripped out
+				
+				$rType	= null;
+				foreach ($regExs as $regEx => $type) {
+					if (preg_match("/".$regEx."/", $data) == 1) {
+						$rValue	= $regEx;
+						$rType	= $type;
+						break;
+					}
+				}
+			}
 			if ($rValue == "Do you want to see the software license\?") {
 				//we are the only ones with the information needed to clear the prompt
 				//if we dont clear it here the Destination function will have a hell of a time figuring out whats going on
 				$strCmd	= "n";
 				$regEx	= "(" . implode("|", array_keys($regExs)) . ")";
+				$cmdObj		= $ctrlObj->getCmd($strCmd, $regEx, $timeout);
+				$cmdObj->get();
+				$data		= $cmdObj->getReturnData(); //need return data so the prompt is not stripped out
+				
+				$rType	= null;
+				foreach ($regExs as $regEx => $type) {
+					if (preg_match("/".$regEx."/", $data) == 1) {
+						$rValue	= $regEx;
+						$rType	= $type;
+						break;
+					}
+				}
+			}
+			//there can be both a license and a forced password change one after the other
+			if ($rValue == "new password\>") {
+				//MT forcing password change, deny the change
+				$strCmd		= chr(3);
+				$regEx	= "(" . implode("|", array_keys($regExs)) . ")";
 				$ctrlObj->getCmd($strCmd, $regEx, $timeout)->get();
 			}
-
+			
 			return $this->getDestinationShell($ctrlObj, $userName);
 			
-		} elseif ($rType == "error") {
+		} 
+		
+		if ($rType == "error") {
 			throw new \Exception("Connect error: " . $rValue);
 		} else {
 			throw new \Exception("Not Handled: " . $rType);
